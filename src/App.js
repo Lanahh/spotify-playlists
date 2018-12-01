41585784413b232905221f21112bee3636532f1a
Lanahh <iloveitsdev@gmail.com>
2018-11-30 20:08:16 -0500

    Added fetching playlists, songs, image, and filter

--- src/App.js
+++ src/App.js
@@ -3,48 +3,48 @@ import './App.css';
 import queryString from 'query-string';
 
 // eslint-disable-next-line 
-let fakeServerData = {
-  user: {
-    name: "Steve",
-    playlists: [
-      {
-        name: "Love songs",
-        songs: [
-          { name: "Beat it", duration: 1234 },
-          { name: "I like you", duration: 1234 }
-        ]
-      },
-      {
-        name: "Hit songs",
-        songs: [
-          { name: "Last Christmas", duration: 1234 },
-          { name: "All I Want For Christmas Is You", duration: 1234 }
-        ]
-      },
-      {
-        name: "Top songs",
-        songs: [
-          { name: "Last Christmas", duration: 1234 },
-          { name: "All I Want For Christmas Is You", duration: 1234 }
-        ]
-      },
-      {
-        name: "Hit songs",
-        songs: [
-          { name: "Last Christmas", duration: 1234 },
-          { name: "All I Want For Christmas Is You", duration: 1234 }
-        ]
-      },
-      {
-        name: "Seasons songs",
-        songs: [
-          { name: "Fall", duration: 2000 },
-          { name: "Summer", duration: 1000 }
-        ]
-      }
-    ]
-  }
-}
+// let fakeServerData = {
+//   user: {
+//     name: "Steve",
+//     playlists: [
+//       {
+//         name: "Love songs",
+//         songs: [
+//           { name: "Beat it", duration: 1234 },
+//           { name: "I like you", duration: 1234 }
+//         ]
+//       },
+//       {
+//         name: "Hit songs",
+//         songs: [
+//           { name: "Last Christmas", duration: 1234 },
+//           { name: "All I Want For Christmas Is You", duration: 1234 }
+//         ]
+//       },
+//       {
+//         name: "Top songs",
+//         songs: [
+//           { name: "Last Christmas", duration: 1234 },
+//           { name: "All I Want For Christmas Is You", duration: 1234 }
+//         ]
+//       },
+//       {
+//         name: "Hit songs",
+//         songs: [
+//           { name: "Last Christmas", duration: 1234 },
+//           { name: "All I Want For Christmas Is You", duration: 1234 }
+//         ]
+//       },
+//       {
+//         name: "Seasons songs",
+//         songs: [
+//           { name: "Fall", duration: 2000 },
+//           { name: "Summer", duration: 1000 }
+//         ]
+//       }
+//     ]
+//   }
+// }
 class PlaylistsCounter extends Component {
   render() {
     return (
@@ -88,14 +88,16 @@ class Playlist extends Component {
   render() {
     let playlist = this.props.playlist
     return (
-      <div style={{ display: 'inline-block', width: "25%" }}>
-        <img alt="" src={playlist.imageUrl} style={{ width: '60px' }} />
-        <h3>{playlist.name}</h3>
-        <ul>
-          {playlist.songs.map(song =>
-            <li>{song.name}</li>
-          )}
-        </ul>
+      <div style={{ width: '50%' ,display: 'inline-block'}}>
+        <span style={{margin:"30px"}}> 
+          <h3>{playlist.name}</h3>          
+          <img alt="" src={playlist.imageUrl} style={{ width: '60px' }} />
+          <ul style={{padding:"30px"}}>
+            {playlist.songs.map(song =>
+              <li>{song.name}</li>
+            )}
+          </ul>
+        </span>
       </div>
     );
   }
@@ -126,13 +128,36 @@ class App extends Component {
     fetch('https://api.spotify.com/v1/me/playlists', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
     }).then(response => response.json())
-      .then(data => this.setState({
-        playlists: data.items.map(item => {
-          console.log(data.items)
+      .then(playlistData => {
+        let playlists = playlistData.items
+        let trackDataPromises = playlists.map(playlist => {
+          let responsePromise = fetch(playlist.tracks.href, {
+            headers: { 'Authorization': 'Bearer ' + accessToken }
+          })
+          let trackDataPromise = responsePromise
+            .then(response => response.json())
+          return trackDataPromise
+        })
+        let allTracksDataPromises = Promise.all(trackDataPromises)
+        let playlistsPromise = allTracksDataPromises.then(trackDatas => {
+          trackDatas.forEach((trackData, i) => {
+            playlists[i].trackDatas = trackData.items
+              .map(item => item.track)
+              .map(trackData => ({
+                name: trackData.name,
+                duration: trackData.duration_ms / 1000
+              }))
+          })
+          return playlists
+        })
+        return playlistsPromise
+      })
+      .then(playlists => this.setState({
+        playlists: playlists.map(item => {
           return {
             name: item.name,
             imageUrl: item.images[0].url,
-            songs: []
+            songs: item.trackDatas.slice(0, 3)
           }
         })
       }))
@@ -142,10 +167,13 @@ class App extends Component {
     let playlistToRender =
       this.state.user &&
         this.state.playlists
-        ? this.state.playlists.filter(playlist =>
-          playlist.name.includes(
-            this.state.filterString))
-        : [] 
+        ? this.state.playlists.filter(playlist =>{
+          let matchesPlaylist = playlist.name.toLowerCase().includes(
+            this.state.filterString.toLowerCase()) 
+          let matchesSong = playlist.songs.find(song => song.name.toLowerCase()
+            .includes(this.state.filterString.toLowerCase()))
+          return matchesPlaylist || matchesSong
+        }) : []
     return (
       <div className="App">
         {this.state.user ?
@@ -153,20 +181,20 @@ class App extends Component {
             <h1 className="App-header">
               {this.state.user.name}'s Favorite Playlists
             </h1>
-            <div style={{ 'margin-left': '10%' }}>
+            <div style={{ 'marginLeft': '10%' }}>
               <PlaylistsCounter playlists={playlistToRender} />
               <HoursCounter playlists={playlistToRender} />
               <Filter onTextChange={text => {
                 this.setState({ filterString: text })
               }} />
-              {playlistToRender.map(playlist =>
-                <Playlist playlist={playlist} />
+              {playlistToRender.map(item =>
+                <Playlist playlist={item} />
               )}
 
             </div>
           </div> : <button onClick={() => {
             window.location = window.location
-            .href.includes('localhost')
+              .href.includes('localhost')
               ? 'http://localhost:8888/login'
               : 'https://oauth-spotify-template.herokuapp.com/login'
           }
